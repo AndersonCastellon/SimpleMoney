89694039035d84036f32a9ae4eac23940c64deac
anderson <anderson.ivan.fuentes@gmail.com>
2019-03-25 16:53:10 -0600

    Creacion entidad Transactions y clase para las constantes

---
+++ app/src/main/java/com/devappan/simplemoney/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.devappan.simplemoney.constants;
+
+public class Constants {
+    public static final String NAME_TABLE_TRANSACTIONS = "transactions";
+    public static final String NAME_COLUMN_ID = "id";
+    public static final String NAME_COLUMN_NAME = "name";
+    public static final String NAME_COLUMN_DESCRIPTION = "description";
+    public static final String NAME_COLUMN_TYPE = "tipe";
+    public static final String NAME_COLUMN_DATATIME = "datatime";
+    public static final String NAME_COLUMN_AMOUNT = "amount";
+}
---
+++ app/src/main/java/com/devappan/simplemoney/db/entity/Transactions.java
@@ -0,0 +1,72 @@
+package com.devappan.simplemoney.db.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import com.devappan.simplemoney.constants.Constants;
+
+@Entity(tableName = Constants.NAME_TABLE_TRANSACTIONS)
+public class Transactions {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = Constants.NAME_COLUMN_ID)
+    private int id;
+    @ColumnInfo(name = Constants.NAME_COLUMN_NAME)
+    private String name;
+    @ColumnInfo(name = Constants.NAME_COLUMN_DESCRIPTION)
+    private String description;
+    @ColumnInfo(name = Constants.NAME_COLUMN_TYPE)
+    private String type;
+    @ColumnInfo(name = Constants.NAME_COLUMN_DATATIME)
+    private int datatime;
+    @ColumnInfo(name = Constants.NAME_COLUMN_AMOUNT)
+    private float amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+
+    public int getDatatime() {
+        return datatime;
+    }
+
+    public void setDatatime(int datatime) {
+        this.datatime = datatime;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+}
