641785563eae6f3db7ea9bd124718b691f63069a
anderson <anderson.ivan.fuentes@gmail.com>
2019-03-27 06:41:49 -0600

    Preparando DAO con Rxjava

--- app/src/main/java/com/devappan/simplemoney/db/dao/TransactionsDAO.java
+++ app/src/main/java/com/devappan/simplemoney/db/dao/TransactionsDAO.java
@@ -5,6 +5,9 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
 
 import com.devappan.simplemoney.db.entity.Transaction;
 
@@ -14,20 +17,20 @@ import java.util.List;
 public interface TransactionsDAO {
 
     @Insert
-    void setTransaction(Transaction transaction); //Insertar una transaccion en la db
+    Completable setTransaction(Transaction transaction); //Insertar una transaccion en la db
 
     @Query("SELECT * FROM transactions")
-    List<Transaction> getAll(); //Leer todas las transacciones de la db
+    Flowable<List<Transaction>> getAll(); //Leer todas las transacciones de la db
 
     @Query("SELECT * FROM transactions WHERE id LIKE :id")
-    Transaction get(int id); //Obtener una transaccion para ver sus detalles / duplicarla
+    Maybe<Transaction> get(int id); //Obtener una transaccion para ver sus detalles / duplicarla
 
     @Update
-    void updateTransaction(Transaction transaction); //Actualizar una transaccion
+    Completable updateTransaction(Transaction transaction); //Actualizar una transaccion
 
     @Query("DELETE FROM transactions")
-    void deleteAll(); //Eliminar todas las transacciones de la db
+    Completable deleteAll(); //Eliminar todas las transacciones de la db
 
     @Delete
-    void deleteTransaction(Transaction transaction); //Eliminar una transaccion especifica
+    Completable deleteTransaction(Transaction transaction); //Eliminar una transaccion especifica
 }
